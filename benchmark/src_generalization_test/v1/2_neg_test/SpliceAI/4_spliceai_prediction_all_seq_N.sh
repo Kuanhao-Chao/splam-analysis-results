cf9bab3c3030f9f752408e1854b2df9374dd7f9c
Kuan-Hao Chao <kuanhao.chao@gmail.com>
2023-07-14 16:38:23 -0400

    Move analysis results into analysis github repo

---
+++ benchmark/src_generalization_test/v1/2_neg_test/SpliceAI/4_spliceai_prediction_all_seq_N.sh
@@ -0,0 +1,9 @@
+TARGET=$1
+SPLICEAI_VERSION=$2
+for (( c=1000; c<=50000; c+=1000 ))
+do
+    echo ">> python 4_spliceai_prediction_all_seq.py $c N $TARGET $SPLICEAI_VERSION"
+    python 4_spliceai_prediction_all_seq.py $c N $TARGET $SPLICEAI_VERSION
+    echo \n\n
+done
+ 
