6c44c6c5f2b43cb7ef819cc8f4e1184288d05924
am12 <pebblealan@gmail.com>
2023-07-20 02:04:21 -0400

    generated new histogram plots :yum:

--- benchmark/src_generalization_test/figures/histograms_new/plot.py
+++ benchmark/src_generalization_test/figures/histograms_new/plot.py
@@ -28,16 +28,14 @@ def plot_hist(type, dataset):
     # defining paper figure for comparing SPLAM and SpliceAI in histogram across all species for donor and acceptor
     
     # data gathering
-    # species = ['GRCm39', 'Mmul_10', 'NHGRI_mPanTro3', 'TAIR10']
     species = ['NHGRI_mPanTro3', 'GRCm39', 'TAIR10']
     species_titles = [r'Pan troglodytes', r'Mus musculus', r'Arabidopsis thaliana']
     sites = ['d_splam', 'd_spliceai', 'a_splam', 'a_spliceai']
-    
     cols = [x+'_'+y for x in species for y in sites]
     all_species_df = pd.DataFrame()
 
     for idx, db in zip(range(0, len(cols), 3), species):
-        avg_ofp = f'../output/aggregate/avg_data.{type}.{db}.csv'
+        avg_ofp = f'../../{dataset}_test/6_output/combine/averaged_data.{type}.{db}.csv'
         df = pd.read_csv(avg_ofp)
 
         t_df = pd.DataFrame(columns=cols[idx:idx+4])
@@ -51,22 +49,15 @@ def plot_hist(type, dataset):
 
     # plot setup
     sns.set(font_scale=0.8)
-    # sns.set_style("whitegrid")
     sns.set_style("whitegrid", {'grid.linestyle': '--'})
-    # sns.set_palette('whitegrid')
     f, axs = plt.subplots(2, 3, figsize=(16, 10), sharey=True, sharex=True)
     plt.subplots_adjust(hspace=None,wspace=None)
-    #f.suptitle('Comparison of SPLAM performance across non-human species', y=0.95, size=12)
     f.supylabel('Density', size=17, weight=400)
     f.supxlabel('Score', size=17, weight=400)
     #plt.rcParams['font.family'] = 'Arial'
 
     # calculate quartiles
     quartiles = all_species_df.quantile([0.25, 0.50, 0.75], axis=0)
-    # diff = pd.DataFrame(quartiles.loc[0.75] - quartiles.loc[0.25]).T
-    # quartiles = pd.concat([quartiles, diff])
-    print(quartiles)
-    # quartiles.to_csv(f'./output/quartiles_{type}.csv')
 
     # traverse subplots column-major (top-down, left-right)
     colors = ["#2ca02c", "#ff7f0e"]
@@ -74,18 +65,16 @@ def plot_hist(type, dataset):
     sns.set_palette(sns.color_palette(colors))
     axs = axs.flatten(order='F')
     for i in range(0,12,2):
+
+        # plot first time to obtain lines
         data = all_species_df.iloc[:,i:i+2]
         ax = axs[i//2]
-
-
         lines = sns.kdeplot(data=data, ax=ax, clip=(0.0, 1.0)).get_lines()
-        x1, y1 = lines[0].get_data() # SpliceAI - orange
-        x2, y2 = lines[1].get_data() # SPLAM - blue
-        #print(max(y1), max(y2))
-        p = sns.kdeplot({'splam': data.iloc[:,0], 'SpliceAI': data.iloc[:,1]}, ax=ax, clip=(0.0, 1.0), fill=True, alpha=0.35, label=["SpliceAI", "splam"]).get_legend().remove()
+        x1, y1 = lines[0].get_data()
+        x2, y2 = lines[1].get_data() 
 
-        # ax.legend(['SpliceAI-10k-noN', 'SPLAM'], loc='upper left', fontsize=10)
-        # sns.move_legend(p, 'upper left')
+        # re-plot with fill
+        p = sns.kdeplot({'splam': data.iloc[:,0], 'SpliceAI': data.iloc[:,1]}, ax=ax, clip=(0.0, 1.0), fill=True, alpha=0.35, label=["SpliceAI", "splam"]).get_legend().remove()
         ax.tick_params(axis='x', labelsize=10)
 
         # plot quartiles
@@ -106,10 +95,7 @@ def plot_hist(type, dataset):
             # plt.rcParams['text.usetex'] = True
             axs[i].set_title(species_titles[i//2], size=14, weight=500, style='italic')
 
-    # ax.legend(['SpliceAI', 'SPLAM'], loc='upper left', fontsize=10)
-
     handles, labels = ax.get_legend_handles_labels()
-    print("handles: ", handles)
     order = [1,0]
     f.legend([handles[idx] for idx in order], ["Splam", "SpliceAI"], loc='upper center', ncol=2, fontsize="15")
 
@@ -121,12 +107,9 @@ def plot_hist(type, dataset):
     axs[0].tick_params(axis='y', labelsize=12, width=12)
     axs[1].tick_params(axis='y', labelsize=12, width=12)
 
-
     # display and save
     plt.tight_layout(rect=(0, 0, 1, 0.95))
-    # plt.show()
-
-    save_fig(f'../figures/fig7/all_scores_compare.{type}.png')
+    save_fig(f'./{dataset}_scores_compare_new.{type}.png')
 
 
 if __name__ == '__main__':
